bc26b25deddf2d2476ffd9add1f5d44024b46525
haider000 <mohd.b.haider000786@gmail.com>
2019-08-18 14:15:27 +0530

    pep8

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views.generic.edit import FormView
 from .forms import SignUpForm
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
-from django.views.generic import TemplateView
-from django.urls import reverse_lazy
 from django.contrib import messages
 
 
@@ -27,11 +24,12 @@ class SignUpView(FormView):
             raw_password = form.cleaned_data['password1']
             form.save()  # saves the form data
             # authenticates the user info
-            user = authenticate(username=username, password=raw_password)
-            user.set_password(raw_password)  # will set the password
+            user = authenticate(username=username,
+                                password=raw_password)
+            user.set_password(raw_password)
             user.save()  # save the user in the user table
-            # this will show an alert if the account is created succesfully
-            messages.success(request, f'Account created for {username}')
+            # this will show an alert if the account is created
+            messages.success(request, f'Account created for{username}')
             if user is not None:
                 if user.is_active:
                     login(request, user)
